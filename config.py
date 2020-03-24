c8c100cda7ec55c309abb70649b965f76f5a48f7
guanggong2020 <guanggong2020@outlook.com>
2020-03-24 21:11:03 +0800

    test upload project to github

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import datetime as dt
 # -------------------参数配置----------------- #
 class Arg:
     def __init__(self):
-       self.filename = './data/'
-       self.date1 = '2019-01-01'
-       self.date2 = date2 = getCurrentTime()
-       self.currenttime = dt.datetime.now().strftime('%Y-%m-%d')
+        self.filename = './data/'
+        self.date1 = '2019-01-01'
+        self.date2 = date2 = getCurrentTime()
+        self.current = dt.datetime.now().strftime('%Y-%m-%d')
